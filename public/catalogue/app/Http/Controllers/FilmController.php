4dbfe58b8354d9264c83cf75bd321123ba1c0d98
HaoJi <jihao4037@gmail.com>
2021-11-19 14:27:57 +0000

    202111191527

---
+++ public/catalogue/app/Http/Controllers/FilmController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Film;
+
+class FilmController extends Controller
+{
+    public function showAllFilms(){
+        $films = Film::with('category')->get();
+        //$authUser = Auth::user();
+        //$favs = $authUser->load('favs')['favs'];
+        //$favIds = array();
+        //foreach($favs as $favs){
+        //    array_push($favIds, $fav->id);
+        //}
+        $data = [
+            "films"=>$films,
+            //"favs"=>$favs,
+            //"favIds"=>$favIds,
+            //"idUser"=> Auth::id(),
+        ];
+        return view('films',$data);
+    }
+    public function select(){
+        Film::all();
+        Film::select("name","direction","categories.name")
+            ->join("categories","films","=","categories.id");
+    }
+}
