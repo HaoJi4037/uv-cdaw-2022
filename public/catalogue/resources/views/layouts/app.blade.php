081fdc43a4b32ebc9946426ea8d99971fafee813
HaoJi <jihao4037@gmail.com>
2021-11-23 09:11:52 +0000

    202111231011 jalon3

--- public/catalogue/resources/views/layouts/app.blade.php
+++ public/catalogue/resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @livewireStyles
 
         <!-- Scripts -->
-        <script src="{{ asset(mix('js/app.js')） }}" defer></script>
+        <script src="{{ asset(mix('js/app.js')) }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
