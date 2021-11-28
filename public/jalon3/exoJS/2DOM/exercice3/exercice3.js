e09726024b8615e475048fdda0d9331047ca7614
HaoJi <jihao4037@gmail.com>
2021-11-28 21:43:34 +0000

    jalon3 exoJS

---
+++ public/jalon3/exoJS/2DOM/exercice3/exercice3.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function modify(e)
+{
+    alert(e.type +" on modify for "+ e.currentTarget.parentNode.id+" !");
+}
+
+function deleter(e)
+{
+    alert(e.type +" on remove for "+ e.currentTarget.parentNode.id+" !");
+}
+
+document.getElementById("addNew").addEventListener("click", function(e){
+    alert(e.type +" on add !");
+})
+
+let modifiers = document.getElementsByClassName("modify");
+Array.from(modifiers).forEach(m => m.addEventListener("click",modify));
+
+let remover = document.getElementsByClassName("remove");
+Array.from(remover).forEach(m => m.addEventListener("click",deleter));
